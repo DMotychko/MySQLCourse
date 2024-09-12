75386b4670eff6d2f17ffb230c6fc05892d7239c
Denis Motichko <motichkodenis10@gmail.com>
2024-09-12 13:26:24 +0200

    Added lesson2 notes

--- lesson2/lesson2.sql
+++ lesson2/lesson2.sql
@@ -20,3 +20,19 @@ select client.*, CreditState from client
 join application on client.idClient = application.Client_idClient
 join department d on client.Department_idDepartment = d.idDepartment
 where Sum > 10000;
+
+insert into users value (null,'Max', 20, 3);
+
+select distinct name from users;
+
+select avg(age) from users
+union
+select sum(age) from users;
+
+select concat('My name is ', name, ' I`m - ', age) from users;
+select * from users where concat('My name is ', name, ' I`m - ', age) = 'My name is Max I`m - 18';
+
+select d.* from client
+                    join application on client.idClient = application.Client_idClient
+                    join denis55.department d on client.Department_idDepartment = d.idDepartment
+where FirstName = 'roman';
