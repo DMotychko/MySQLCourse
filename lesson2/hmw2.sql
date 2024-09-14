bd86a7b1cab03242ddf6f93134d8679fb2e659e6
Denis Motichko <motichkodenis10@gmail.com>
2024-09-14 17:59:32 +0200

    Added homework for lesson2

--- lesson2/hmw2.sql
+++ lesson2/hmw2.sql
@@ -15,7 +15,7 @@ where DepartmentCity = 'lviv';
 select distinct client.FirstName from client;
 
 select (select count(*) from client) as total_client,
-       (select count(*) from client
+        (select count(*) from client
         join denis55.department d on d.idDepartment = client.Department_idDepartment
         where DepartmentCity = 'Lviv') as Lviv_client;
 
@@ -32,3 +32,40 @@ select max(sum) as maxCredit, min(sum) as minCredit from application;
 select count(*) from application
 join denis55.client c on c.idClient = application.Client_idClient
 where c.Education = 'high';
+
+select avg(Sum) as avg, FirstName from application
+join denis55.client c on c.idClient = application.Client_idClient
+group by FirstName
+order by avg desc limit 1;
+
+select sum(Sum) as sum, DepartmentCity from department
+join denis55.client c on department.idDepartment = c.Department_idDepartment
+join denis55.application a on c.idClient = a.Client_idClient
+group by DepartmentCity
+order by sum desc limit 1;
+
+select max(Sum) as maxSum, DepartmentCity from department
+join denis55.client c on department.idDepartment = c.Department_idDepartment
+join denis55.application a on c.idClient = a.Client_idClient
+group by DepartmentCity
+order by maxSum desc limit 1;
+
+update client
+join denis55.application a on client.idClient = a.Client_idClient
+set Sum = 6000
+where client.Education = 'high';
+
+update client
+join denis55.department d on d.idDepartment = client.Department_idDepartment
+set City = 'Kyiv'
+where DepartmentCity = 'Kyiv';
+
+delete application from application where CreditState = 'returned';
+
+delete application from application
+join denis55.client c on c.idClient = application.Client_idClient
+where LastName like '_a%' or
+      LastName like '_e%' or
+      LastName like '_y%' or
+      LastName like '_u%' or
+      LastName like '_o%';
